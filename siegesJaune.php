6dc3b8e35aba44db01d2ff5f111ab3bdbd723bb9
Matheo-20 <144718634+Matheo-20@users.noreply.github.com>
2023-12-06 13:40:37 +0100

    Add files via upload

---
+++ siegesJaune.php
@@ -0,0 +1,11 @@
+<?php
+echo "sieges jaunes :\n\n" ;
+
+for( $numSiege = 1 ; $numSiege <= 40 ; $numSiege = $numSiege + 1 ){
+	if ( $numSiege % 3 ==  0) {
+		echo "Siége numéro " ,  $numSiege , "\n" ; 
+	}
+}
+
+
+ 
